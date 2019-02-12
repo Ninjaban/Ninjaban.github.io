64f351858df9e9762a6d4089f9d32500b059dd78
Jonathan Carra <jcarra@student.42.fr>
2019-02-12 12:09:01 +0100

    update

--- sources/script/load-marker.js
+++ sources/script/load-marker.js
@@ -14,6 +14,6 @@ geojson.features.forEach(function(marker) {
     new mapboxgl.Marker(el)
 	.setLngLat(marker.geometry.coordinates)
 	.setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-	.setHTML('<h3>' + marker.properties.name + '</h3><ul><li>' + marker.properties.mmsi + '</li><li>MMSI: ' + marker.properties.mmsi + '</li><li>IMO: ' + marker.properties.imo + '</li><li>Statut: ' + marker.properties.status + '</li><li>Time: ' + marker.properties.timestamp + '</li><li>Pavillion: ' + marker.properties.flag + '</li><li>Longueur: ' + marker.properties.lenght + '</li><li>Largeur: ' + marker.properties.width + '</li><li>Année de construction: ' + marker.properties.year + '</li><li>Type: ' + marker.properties.type + '</li><li>Destination: ' + marker.properties.dest + '</li><li>Distance restante: ' + marker.properties.dtg + '</li><li>Distance effectué' + marker.properties.dtt + '</li><li>ETA: ' + marker.properties.eta + '</li></ul>'))
+	.setHTML('<h3>' + marker.properties.name + '</h3><ul><li>MMSI: ' + marker.properties.mmsi + '</li></ul>'))
 	.addTo(map);
 });
