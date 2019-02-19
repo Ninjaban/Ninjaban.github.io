07195b85895dddf11ec5cebb412c3a645939f28a
Jonathan Carra <jcarra@student.42.fr>
2019-02-19 16:26:48 +0100

    update

--- slider-value.js
+++ slider-value.js
@@ -1,7 +1,7 @@
 var slideCol = document.getElementById("myRange");
 var y = document.getElementById("f");
-y.innerHTML = slideCol.value;
+y.innerHTML = slideCol.value + '%';
 
 slideCol.oninput = function() {
-    y.innerHTML = this.value;
+    y.innerHTML = this.value + '%';
 }
