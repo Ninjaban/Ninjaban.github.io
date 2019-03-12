d74c3f96b5e4c40a6c289e18572da5275020dd74
Jonathan Carra <jcarra@student.42.fr>
2019-03-12 14:54:09 +0100

    update

--- test/create.php
+++ test/create.php
@@ -1,21 +1,19 @@
 <?php
-
 $submit = $_POST["submit"];
-if (($submit === "OUI" || $submit === "NON") && $_POST["id"] != NULL) {
-   $account = array();
-
-   if (file_exists("sources/") == false)
-      mkdir("sources/");
-
-   if ($submit === "OUI")
-      $key = ",1"
-   else
-      $key = ",0"
-
-   $line = $_POST["id"].$key.'\n';
-
-   echo "OK\n";
+if (($submit === "OUI" || $submit === "NON") && $_POST["id"] != NULL)
+{
+$account = array();
+if (file_exists("sources/") == false)
+mkdir("sources/");
+if ($submit === "OUI")
+{
+$key = ",1";
 }
 else
-   echo "ERROR\n";
+$key = ",0";
+$line = $_POST["id"].$key.'\n';
+echo "OK\n";
+}
+else
+echo "ERROR\n";
 ?>
