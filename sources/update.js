926eee1feca60f1ac9a293b8f30de34971699e3c
Jonathan Carra <jcarra@student.42.fr>
2019-03-12 16:18:35 +0100

    update

--- sources/update.js
+++ sources/update.js
@@ -6,24 +6,6 @@ var data
 
 
 
-function openfile(file, string) {
-    console.log(document.location.toString() + file);
-    var mywindow = window.open(document.location.toString() + file, "", 'toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=10000, top=10000, width=10, height=10, visible=none');
-    mywindow.document.writeln(string + "\n");
-}
-
-function button_oui() {
-    openfile("sources/retour.html", this.name + ",1");
-}
-
-function button_non() {
-    openfile("sources/retour.html", this.name + ",0");
-}
-
-
-
-
-
 function collision_read() {
     var head= document.getElementsByTagName('head')[0];
     if (status == 1) {
