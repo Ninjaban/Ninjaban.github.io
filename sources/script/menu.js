eb5de45dd25d27b16bbfe414d2c355980e177097
Jonathan Carra <jcarra@student.42.fr>
2019-04-08 18:17:29 +0200

    update try

--- sources/script/menu.js
+++ sources/script/menu.js
@@ -18,6 +18,6 @@ function menu_click(elem) {
     if (id == "menu-map" && className != "br-menu-link active")
     {
         document.getElementById('nav-main').style = "display: none";
-        document.getElementById('nav-map').style = "display: block";
+        document.getElementById('nav-map').style = "display: ";
     }
 };
