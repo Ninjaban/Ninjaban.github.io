5b690e607fdc5ca8f19bc2e30ec069f967c4905f
Jonathan Carra <jcarra@student.42.fr>
2019-04-08 14:09:53 +0200

    try update

--- sources/script/menu.js
+++ sources/script/menu.js
@@ -9,12 +9,12 @@ function menu_click() {
             all[i].className = "br-menu-link active";
     }
 
-    if (id === "menu-main" && className !== "br-menu-link active")
+    if (id == "menu-main" && className != "br-menu-link active")
     {
         document.getElementById('nav-main').style = "display: block";
         document.getElementById('nav-map').style = "display: none";
     }
-    if (id === "menu-map" && className !== "br-menu-link active")
+    if (id == "menu-map" && className != "br-menu-link active")
     {
         document.getElementById('nav-main').style = "display: none";
         document.getElementById('nav-map').style = "display: block";
