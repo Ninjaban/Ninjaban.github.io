dfef7600990a5f1dcf88b0fd25381c9002820089
Jonathan Carra <jcarra@student.42.fr>
2019-12-11 15:25:09 +0100

    Updates

---
+++ precache-manifest.582845ab6286388785b66a4fa4b32b91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f423638d6c0c953baab3e6aeaae5c5d7",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "bcedbf2b70f4539bd422",
+    "url": "/dashboard/static/css/main.0cbf4080.chunk.css"
+  },
+  {
+    "revision": "1cff9ea409ba6f7bb1b4",
+    "url": "/dashboard/static/js/2.cc8b3e23.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/dashboard/static/js/2.cc8b3e23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcedbf2b70f4539bd422",
+    "url": "/dashboard/static/js/main.7066e212.chunk.js"
+  },
+  {
+    "revision": "9654e981475cd9a6fea2",
+    "url": "/dashboard/static/js/runtime-main.c59dab00.js"
+  },
+  {
+    "revision": "bba900a1579c6597dc6bf91f4ccd9558",
+    "url": "/dashboard/static/media/LogoSimple.bba900a1.png"
+  }
+]);
